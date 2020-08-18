729307dc31f5bef1efac0f1f16ce1ffaad9f3630
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2020-08-18 08:53:44 +0200

    feat(gaussian): GaussianBackend with phaseshift
    - All the Gaussian-related stuff has been moved under
      `piquasso.gaussian` for better separation of different backends.
    
    - The `GaussianBackend.phaseshift` has been implemented. Docstrings have
      also been written for Sphinx.
    
    - Tests have been written under `piquasso.gaussian.tests`, expecting a
      proper phaseshift on the `C`, `G` matrices and `mean` vector.
    
    - The `.gitignore` file has been extended with Sphinx-related ignores:
      when the developer re-generated the HTML files, changes were added
      to some `*.rst` files under docs, which are actually not needed at
      this point, they are always re-generated.
    
    - The `flake8` linter should ignore the `W605` warnings due to the
      LaTeX-containing docstrings. This has been specified in the `.flake8`
      file. Also, the `E201` warning is also ignored in order to enable
      writing aligned matrices.
    
    Issue: #8

---
+++ piquasso/gaussian/tests/test_backend.py
@@ -0,0 +1,70 @@
+#
+# Copyright (C) 2020 by TODO - All rights reserved.
+#
+
+import pytest
+import numpy as np
+
+from ..backend import GaussianBackend
+from ..state import GaussianState
+
+
+@pytest.fixture(scope="session")
+def tolerance():
+    return 10E-10
+
+
+class TestGaussianBackend:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        C = np.array(
+            [
+                [3, 1, 0],
+                [1, 2, 0],
+                [0, 0, 1],
+            ],
+            dtype=complex,
+        )
+        G = np.array(
+            [
+                [1, 3, 0],
+                [3, 2, 0],
+                [0, 0, 1],
+            ],
+            dtype=complex,
+        )
+        mean = np.ones(3, dtype=complex)
+
+        state = GaussianState(C, G, mean)
+
+        self.backend = GaussianBackend(state=state)
+
+    def test_phaseshift(self, tolerance):
+        angle = np.pi/3
+        self.backend.phaseshift(params=(angle,), modes=(0,))
+
+        phase = np.exp(1j * angle)
+
+        expected_C = np.array(
+            [
+                [    3, np.conj(phase), 0],
+                [phase,              2, 0],
+                [    0,              0, 1],
+            ],
+            dtype=complex,
+        )
+        expected_G = np.array(
+            [
+                [-np.conj(phase), 3*phase, 0],
+                [        3*phase,       2, 0],
+                [              0,       0, 1],
+            ],
+            dtype=complex,
+        )
+        expected_mean = np.array([phase, 1, 1], dtype=complex)
+
+        assert (np.abs(self.backend.state.C - expected_C) < tolerance).all()
+        assert (np.abs(self.backend.state.G - expected_G) < tolerance).all()
+        assert (
+            np.abs(self.backend.state.mean - expected_mean) < tolerance
+        ).all()
