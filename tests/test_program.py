0386f31647c0ee613e653d70c693d66998c55db0
Boldizsár Poór <boldar99@gmail.com>
2020-09-19 22:32:50 +0200

    feat: adding BlackBird support
    - Methods have been implemented that enables us to load the `instructions` of a `Program` from:
      - a BlackbirdProgram
      - a file containing a valid BlackbirdProgram
      - a string containing a valid BlackbirdProgram
    - `phaseshift` has been added to `FockBackend` without an implementation.
    - `blackbird_op_to_gate` has been added to `Gate` as a staticmethod, since it is logically connected to this class.
    - A new class: `R` has been implemented as the Rotation or Phaseshifter gate. The name may not be appropriate - we might want to use `P`.
    - A test has been added to `TestProgram` for testing `Program.from_blackbird`

--- tests/test_program.py
+++ tests/test_program.py
@@ -37,3 +37,25 @@ class TestProgram:
 
         assert len(self.program.instructions) == 2
         self.program.backend.execute_instructions.assert_called_once()
+
+    def test_from_blackbird(self):
+        str = \
+            """name StateTeleportation
+            version 1.0
+
+            BSgate(0.7853981633974483, 0) | [1, 2]
+            Rgate(0.7853981633974483) | 1
+            """
+        self.program.loads_blackbird(str)
+
+        assert len(self.program.instructions) == 2
+
+        bs_gate = self.program.instructions[0]
+        assert bs_gate['modes'] == [1, 2]
+        assert bs_gate['params'] == [0.7853981633974483, 0]
+        assert bs_gate['op'] == FockBackend.beamsplitter
+
+        r_gate = self.program.instructions[1]
+        assert r_gate['modes'] == [1]
+        assert r_gate['params'] == [0.7853981633974483]
+        assert r_gate['op'] == FockBackend.phaseshift
