187beb261a1705e6969df9bc0dc8d25dd82e692d
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2020-10-31 22:35:45 +0100

    fix(apply_to_C_and_G): conjugation and indexing
    **Problem**
    
    - A conjugation of the `T` matrix has been missing at `apply_to_C_and_G`
      in the case of applying the matrix to the remaining modes. This is
      because `apply_to_C_and_G` expects a `T` matrix which transforms e.g.
      the expectation value of the annihilation operators. A more detailed
      description of the transformation of vectors of ladder operators can
      be found [in this LaTeX document](https://www.overleaf.com/read/xrkncztkffzv).
    
    - As it turned out, `apply_to_C_and_G` only worked for certain cases,
      e.g. 2 mode gates with 3 mode states, but haven't been noticed without
      proper unit testing.
    
    **Solution**
    
    The computation has been extended with the conjugation. This didn't
    break any of the previous tests, which was expected, but this statement
    alone cries for other tests.
    
    The computation has also been generalized for any `(l, l)` shaped `T`
    matrix, where `l` is a positive integer.
    
    Also, the edge case when there are no remaining modes (i.e. `T` is a
    d×d matrix, where `d` is the number of modes) need to be also
    handled, since one couldn't index a numpy array with an empty index
    array.
    
    The `apply_to_C_and_G` method got too large and complex, so another
    method named `_apply_to_other_modes` got extracted from it, which deals
    with the application of the transformation to the other indices of `C`
    and `G` which are not handled in the caller method `apply_to_C_and_G`.
    
    `rows` and `columns` got renamed to `transformed_rows` and
    `transformed_columns`, and `all_other_*` got renamed to `other_*` for
    brevity.
    
    `TestGaussianState` has been separated into
    `TestGaussianStateRepresentations` and `TestGaussianStateOperations`,
    since the dimension of the two states used for the two test classes
    differ for practical reasons. `TestGaussianStateRepresentations` uses a
    state with `d == 2`, which render the covariance matrix a 4-by-4 matrix,
    and is conveniently written in a fixture.
    Using `d == 3` would make the covariance matrix a `6-by-6` matrix, which
    is much more inconvenient to deal with at testing. However, for
    `TestGaussianStateOperations` using `d == 3` is more sensible,
    especially if we want to test non-trivial cases of `apply_to_C_and_G`.
    
    Several test cases have been written in `TestGaussianStateOperations`
    testing `apply_to_C_and_G`
    - for application to 1 mode only (`T.shape` is `(1, 1)`);
    - for application to 2 modes;
    - for application to all the modes (3);
    - for application to 3 modes, where the modes are `(0, 1, 3)`, i.e. they
      are disjoint. This test alone uses a state with `d == 5`, hence it is
      a `staticmethod`.
    
    The latter is important, since it verifies that `apply_to_C_and_G` can
    run also with disjoint modes with multiple external modes (~
    `all_other_modes`).
    
    For the latter test case, it would be cumbersome to hardcode matrices,
    therefore the following fixtures were created:
    - `generate_symmetric_matrix` for usage in
      `generate_complex_symmetric_matrix`,
    - `generate_complex_symmetric_matrix` for generating a random `G`
      matrix,
    - `generate_unitary_matrix` for usage in `generate_hermitian_matrix` and
      for generating a random transformation in
      `test_apply_to_C_and_G_for_5_modes`,
    - `generate_hermitian_matrix` for generating a random `C` matrix.

--- piquasso/gaussian/tests/test_state.py
+++ piquasso/gaussian/tests/test_state.py
@@ -9,7 +9,7 @@ from piquasso.gaussian.state import GaussianState
 from piquasso import constants
 
 
-class TestGaussianState:
+class TestGaussianStateRepresentations:
 
     @pytest.fixture
     def mean(self):
@@ -96,3 +96,176 @@ class TestGaussianState:
             sigma,
             self.state.sigma,
         )
+
+
+class TestGaussianStateOperations:
+    @pytest.fixture
+    def mean(self):
+        return np.array([1 - 2j, 3 + 4j, 2 - 5j], dtype=complex)
+
+    @pytest.fixture
+    def C(self):
+        return np.array(
+            [
+                [     1, 1 + 2j, 4 - 6j],
+                [1 - 2j,      6, 5 + 9j],
+                [4 + 6j, 5 - 9j,    -10],
+            ],
+            dtype=complex,
+        )
+
+    @pytest.fixture
+    def G(self):
+        return np.array(
+            [
+                [3 + 1j, 1 + 1j, 2 + 2j],
+                [1 + 1j, 2 - 3j, 5 - 6j],
+                [2 + 2j, 5 - 6j,      7]
+            ],
+            dtype=complex,
+        )
+
+    @pytest.fixture(autouse=True)
+    def setup(self, C, G, mean):
+        self.state = GaussianState(C, G, mean)
+
+    def test_apply_to_C_and_G_for_1_modes(self, C, G):
+        alpha = np.exp(1j * np.pi/3)
+
+        T = np.array(
+            [
+                [alpha]
+            ],
+            dtype=complex,
+        )
+
+        expected_C = C.copy()
+
+        expected_C[1, 1] = alpha.conjugate() * C[1, 1] * alpha
+
+        expected_C[1, 0] = alpha.conjugate() * C[1, 0]
+        expected_C[1, 2] = alpha.conjugate() * C[1, 2]
+
+        expected_C[0, 1] = C[0, 1] * alpha
+        expected_C[2, 1] = C[2, 1] * alpha
+
+        expected_G = G.copy()
+
+        expected_G[1, 1] = alpha * G[1, 1] * alpha
+
+        expected_G[1, 0] = alpha * G[1, 0]
+        expected_G[1, 2] = alpha * G[1, 2]
+
+        expected_G[0, 1] = G[0, 1] * alpha
+        expected_G[2, 1] = G[2, 1] * alpha
+
+        self.state.apply_to_C_and_G(T, modes=(1,))
+
+        assert np.allclose(self.state.C, expected_C)
+        assert np.allclose(self.state.G, expected_G)
+
+    def test_apply_to_C_and_G_for_2_modes(self, C, G):
+
+        T = np.array(
+            [
+                [     1, 5 + 6j],
+                [5 - 6j,      7],
+            ],
+            dtype=complex,
+        )
+
+        expected_C = np.zeros((3, 3), dtype=complex)
+
+        expected_C[:2, :2] = T.conjugate() @ C[:2, :2] @ T.transpose()
+
+        expected_C[(0, 1), 2] = T.conjugate() @ C[(0, 1), 2]
+        expected_C[2, (0, 1)] = C[2, (0, 1)] @ T.transpose()
+
+        expected_C[2, 2] = C[2, 2]
+
+        expected_G = np.zeros((3, 3), dtype=complex)
+
+        expected_G[:2, :2] = T @ G[:2, :2] @ T.transpose()
+
+        expected_G[(0, 1), 2] = T @ G[(0, 1), 2]
+        expected_G[2, (0, 1)] = G[2, (0, 1)] @ T.transpose()
+
+        expected_G[2, 2] = G[2, 2]
+
+        self.state.apply_to_C_and_G(T, modes=(0, 1))
+
+        assert np.allclose(self.state.C, expected_C)
+        assert np.allclose(self.state.G, expected_G)
+
+    def test_apply_to_C_and_G_for_all_modes(self, C, G):
+        T = np.array(
+            [
+                [       1,    3 + 4j,   9 - 10j],
+                [  3 - 4j,         7, -11 + 12j],
+                [ 9 + 10j, -11 - 12j,        18]
+            ],
+            dtype=complex,
+        )
+
+        expected_C = T.conjugate() @ C @ T.transpose()
+
+        expected_G = T @ G @ T.transpose()
+
+        self.state.apply_to_C_and_G(T, modes=(0, 1, 2))
+
+        assert np.allclose(self.state.C, expected_C)
+        assert np.allclose(self.state.G, expected_G)
+
+    @staticmethod
+    def test_apply_to_C_and_G_for_5_modes(
+        generate_complex_symmetric_matrix,
+        generate_hermitian_matrix,
+        generate_unitary_matrix,
+    ):
+        C = generate_hermitian_matrix(5)
+        G = generate_complex_symmetric_matrix(5)
+        mean = np.random.rand(5) + 1j * np.random.rand(5)
+
+        state = GaussianState(
+            C=C,
+            G=G,
+            mean=mean,
+        )
+
+        expected_C = C.copy()
+        expected_G = G.copy()
+
+        T = generate_unitary_matrix(3)
+
+        columns = np.array(
+            [
+                [0, 1, 3],
+                [0, 1, 3],
+                [0, 1, 3],
+            ]
+        )
+
+        rows = np.array(
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [3, 3, 3],
+            ]
+        )
+
+        index = rows, columns
+
+        expected_C[index] = T.conjugate() @ expected_C[index] @ T.transpose()
+        expected_C[(0, 1, 3), 2] = T.conjugate() @ expected_C[(0, 1, 3), 2]
+        expected_C[(0, 1, 3), 4] = T.conjugate() @ expected_C[(0, 1, 3), 4]
+        expected_C[:, (0, 1, 3)] = np.conj(expected_C[(0, 1, 3), :]).transpose()
+
+        expected_G[index] = T @ expected_G[index] @ T.transpose()
+        expected_G[(0, 1, 3), 2] = T @ expected_G[(0, 1, 3), 2]
+        expected_G[(0, 1, 3), 4] = T @ expected_G[(0, 1, 3), 4]
+        expected_G[:, (0, 1, 3)] = expected_G[(0, 1, 3), :].transpose()
+
+        state.apply_to_C_and_G(T, modes=(0, 1, 3))
+
+        assert np.allclose(state.C, expected_C)
+        assert np.allclose(state.G, expected_G)
