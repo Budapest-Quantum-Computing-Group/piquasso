a477f6668c05cb6585609ef348afffa129c2cf7c
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2020-11-08 21:30:08 +0100

    refact(test_program): Removing odd dependencies
    **Problem**
    
    The current `TestProgram` test class is not `Backend` or
    `Operation`-independent.
    
    **Solution**
    
    `TestProgram` got refactored with suitable, independent test dummy
    fixtures.
    
    To assert that the `Backend.execute_instructions` is called, a `Mock`
    object has to be created, but `Program` expects a class instead of an
    object. This is resolved with a small trick: a lambda function is passed
    as `backend_class` to the `Program`.
    
    The `blackbird`-related test `test_from_blackbird` is moved to
    `TestBlackbirdParsing` for later resolution.

--- tests/test_program.py
+++ tests/test_program.py
@@ -7,19 +7,48 @@ from unittest.mock import Mock
 import pytest
 
 from piquasso.context import Context
-from piquasso.sampling import SamplingBackend
-from piquasso.operations import B, Sampling
+from piquasso.backend import Backend
 from piquasso.mode import Q
 from piquasso.program import Program
+from piquasso.operations import Operation, ModelessOperation
+
+from piquasso.sampling import SamplingBackend
 
 
 class TestProgram:
     @pytest.fixture(autouse=True)
     def setup(self):
+        class DummyBackend(Backend):
+            def dummy_operation(self, modes, params):
+                pass
+
+            def dummy_modeless_operation(self, params):
+                pass
+
+        self.backend_class = DummyBackend
+
         self.program = Program(
             state=Mock(name="State"),
-            backend_class=lambda _: Mock(SamplingBackend, name="Backend")
+            backend_class=lambda state: Mock(self.backend_class, name="DummyBackend"),
         )
+
+    @pytest.fixture
+    def DummyOperation(self):
+        class _DummyOperation(Operation):
+            backends = {
+                self.backend_class: self.backend_class.dummy_operation,
+            }
+
+        return _DummyOperation
+
+    @pytest.fixture
+    def DummyModelessOperation(self):
+        class _DummyModelessOperation(ModelessOperation):
+            backends = {
+                self.backend_class: self.backend_class.dummy_modeless_operation,
+            }
+
+        return _DummyModelessOperation
 
     def test_current_program_in_program_context(self):
         with self.program:
@@ -27,22 +56,37 @@ class TestProgram:
 
         assert Context.current_program is None
 
-    def test_program_instructions(self):
+    def test_program_instructions(self, DummyOperation):
         assert len(self.program.instructions) == 0
 
         with self.program:
-            Q(0, 1) | B(0.1, 0.4) | B(0.5, 0.3)
+            Q(0, 1) | DummyOperation(42) | DummyOperation(42, 320)
 
         self.program.execute()
 
         assert len(self.program.instructions) == 2
         self.program.backend.execute_instructions.assert_called_once()
 
-    def test_modeless_program_instructions(self):
+    def test_modeless_program_instructions(self, DummyModelessOperation):
         with self.program:
-            Sampling(shots=10)
+            DummyModelessOperation(42)
 
         assert len(self.program.instructions) == 1
+
+
+class TestBlackbirdParsing:
+    """
+    TODO: Temporary solution to test `blackbird` code parsing.
+
+    Ideally, `blackbird` parsing should be done `Backend`-independently.
+    """
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.program = Program(
+            state=Mock(name="State"),
+            backend_class=SamplingBackend,
+        )
 
     def test_from_blackbird(self):
         str = \
