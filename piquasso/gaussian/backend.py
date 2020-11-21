c77cddef9b2ae9be56e166f77c01eda5ea93e1bf
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2020-11-21 23:59:41 +0100

    HOTFIX(gaussian.backend): Displacement gate
    **Problem**
    
    The displacement operation should transform the `C` and `G` matrices as
    well. Previously, when the displacement operator got applied, the
    determinant of the covariance matrix could turn negative, which is
    non-physical: this value should always be non-negative.
    
    **Solution**
    
    The missing transformation of the `C` and `G` matrices got implemented.
    
    Note, that neither `apply` nor `apply_active` could be used for this
    case due to the nature of the displacement.

--- piquasso/gaussian/backend.py
+++ piquasso/gaussian/backend.py
@@ -102,6 +102,16 @@ class GaussianBackend(Backend):
                 = \langle \hat{a}_i + \alpha \mathbb{1}) \rangle_{\rho}
                 = m + \alpha.
 
+        The :math:`\hat{a}_i \mapsto hat{a}_i + \alpha \mathbb{1}` transformation
+        should be applied to the `GaussianState.C` and `GaussianState.G` matrices as
+        well:
+
+        .. math::
+            C_{ii} \mapsto C_{ii} + \alpha m_i^* + \alpha^* m_i + | \alpha |^2, \\
+            G_{ii} \mapsto G_{ii} + 2 \alpha m_i + \alpha^2, \\
+
+        and to the corresponding `i, j (i \neq j)` modes.
+
         Note, that :math:`\alpha` is often written in the form
 
         .. math:
@@ -110,6 +120,10 @@ class GaussianBackend(Backend):
         where :math:`r \geq 0` and :math:`\phi \in [ 0, 2 \pi )`. When two parameters
         are specified for this operation, the first is interpreted as :math:`r`, and the
         second one as :math:`\phi`.
+
+        Also note, that the displacement cannot be categorized as an active or passive
+        linear transformation, because the unitary transformation does not strictly
+        produce a linear combination of the field operators.
 
         Args:
             params (tuple): Parameter(s) for the displacement operation in the form of
@@ -127,7 +141,25 @@ class GaussianBackend(Backend):
 
         mode = modes[0]
 
+        mean_copy = np.copy(self.state.mean)
+
         self.state.mean[mode] += alpha
+
+        self.state.C[mode, mode] += (
+            alpha * np.conj(mean_copy[mode])
+            + np.conj(alpha) * mean_copy[mode]
+            + np.conj(alpha) * alpha
+        )
+
+        self.state.G[mode, mode] += 2 * alpha * mean_copy[mode] + alpha * alpha
+
+        other_modes = np.delete(np.arange(self.state.d), modes)
+
+        self.state.C[other_modes, mode] += alpha * mean_copy[other_modes]
+        self.state.C[mode, other_modes] += np.conj(alpha) * mean_copy[other_modes]
+
+        self.state.G[other_modes, mode] += alpha * mean_copy[other_modes]
+        self.state.G[mode, other_modes] += alpha * mean_copy[other_modes]
 
     def squeezing(self, params, modes):
         r"""
