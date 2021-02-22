6e2694505a9ea70ffbbf9f4bd15491c3217a8f77
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-02-22 12:32:45 +0100

    refact(module=piquasso): Module wrapping
    **Problem**
    
    One might need to override certain classes (e.g. `GaussianState`) in the
    implementation. This can not be achieved simply via the current
    implementation.
    
    **Solution**
    
    The `piquasso` module is wrapped in a wrapper class called `Piquasso`.
    Getting an attribute from the module `piquasso` from now on is done by
    trying to retrieve items from the `registry`. If the retrieval is not
    successful, then the original wrapped module is used for item retrieval.
    
    In test files under `tests`, the `piquasso` module is used instead of
    importing.
    
    With this patch, the `piquasso` module would serve as the public-facing
    interface of `piquasso`.

---
+++ tests/test_plugin.py
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2020 by TODO - All rights reserved.
+#
+
+import numpy as np
+
+import pytest
+
+import piquasso as pq
+
+
+@pytest.fixture
+def MyGaussianBackend():
+    # TODO: better way to expose API
+    class _MyGaussianBackend(pq.gaussian.backend.GaussianBackend):
+        pass
+
+    _MyGaussianBackend.__name__ = "MyGaussianBackend"
+
+    return _MyGaussianBackend
+
+
+@pytest.fixture
+def MyGaussianState(MyGaussianBackend):
+    class _MyGaussianState(pq.GaussianState):
+        _backend_class = MyGaussianBackend
+
+    _MyGaussianState.__name__ = "MyGaussianState"
+
+    return _MyGaussianState
+
+
+@pytest.fixture
+def MyBeamsplitter():
+    class _MyBeamsplitter(pq.B):
+        pass
+
+    _MyBeamsplitter.__name__ = "MyBeamsplitter"
+
+    return _MyBeamsplitter
+
+
+def test_use_plugin(MyGaussianState, MyBeamsplitter, MyGaussianBackend):
+    class Plugin:
+        classes = {
+            "GaussianState": MyGaussianState,
+            "B": MyBeamsplitter,
+        }
+
+    pq.use(Plugin)
+
+    program = pq.Program(state=pq.GaussianState.create_vacuum(3))
+
+    with program:
+        pq.Q(0, 1) | pq.B(theta=np.pi/3)
+
+    program.execute()
+
+    assert program.state.__class__ is MyGaussianState
+    assert program.backend.__class__ is MyGaussianBackend
+    assert pq.B is MyBeamsplitter
+
+
+def test_use_plugin_with_reimport(MyGaussianState, MyBeamsplitter, MyGaussianBackend):
+    class Plugin:
+        classes = {
+            "GaussianState": MyGaussianState,
+            "B": MyBeamsplitter,
+        }
+
+    pq.use(Plugin)
+
+    program = pq.Program(state=pq.GaussianState.create_vacuum(3))
+
+    with program:
+        pq.Q(0, 1) | pq.B(theta=np.pi/3)
+
+    program.execute()
+
+    import piquasso  # noqa: F401
+
+    assert program.state.__class__ is MyGaussianState
+    assert program.backend.__class__ is MyGaussianBackend
+    assert pq.B is MyBeamsplitter
