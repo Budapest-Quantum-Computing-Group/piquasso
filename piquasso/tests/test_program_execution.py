b048ea8b565653f615d400cef363436991fa411d
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-03-06 18:49:46 +0100

    feat(mode): Registration from LHS
    Registraton of operations, states, programs is now made possible from
    the left hand side as well.

--- piquasso/tests/test_program_execution.py
+++ piquasso/tests/test_program_execution.py
@@ -37,3 +37,12 @@ class TestProgramExecution(TestProgramBase):
 
         self.program.circuit.dummy_operation.assert_called_once()
         self.program.circuit.dummy_modeless_operation.assert_called_once()
+
+    def test_register_operation_from_left_hand_side(self, DummyOperation):
+        operation = DummyOperation(420)
+        with self.program:
+            operation | Q(0, 1)
+
+        self.program.execute()
+
+        self.program.circuit.dummy_operation.assert_called_once_with(operation)
