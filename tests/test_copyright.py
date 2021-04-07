1052bee6cbe340ceaa30064d189af82267769b4b
Zoltán Kolarovszki <kolarovszki@inf.elte.hu>
2021-04-07 10:27:37 +0200

    feat: Final version of copyright
    The final copyright has been added to each python files. A test has been
    created under `tests/test_copyright.py` which tests every file for
    having the copyright.

---
+++ tests/test_copyright.py
@@ -0,0 +1,82 @@
+#
+# Copyright 2021 Budapest Quantum Computing Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import glob
+from itertools import islice
+from pathlib import Path
+
+COPYRIGHT = """\
+#
+# Copyright 2021 Budapest Quantum Computing Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+
+def test_copyrights():
+    root = Path(__file__).parents[1]
+
+    exceptions = [
+        root / ".venv",
+        root / "docs",
+        root / "dist",
+    ]
+
+    extensions = ["*.py"]
+
+    for extension in extensions:
+        for filename in glob.iglob(
+            str(root / "**" / extension), recursive=True
+        ):
+            if (
+                _is_current_file(filename)
+                or _is_empty_file(filename)
+                or _in_exceptions(filename, exceptions)
+            ):
+                continue
+
+            with open(filename, "r+") as file:
+                first_few_lines = "".join(
+                    islice(file.readlines(), COPYRIGHT.count("\n"))
+                )
+
+            assert (
+                COPYRIGHT in first_few_lines
+            ), f"Invalid or no copyright found in {filename}"
+
+
+def _is_current_file(filename):
+    return filename == os.path.abspath(__file__)
+
+
+def _is_empty_file(filename):
+    return os.stat(filename).st_size == 0
+
+
+def _in_exceptions(filename, exceptions):
+    for exception in exceptions:
+        if Path(exception) in Path(filename).parents:
+            return True
