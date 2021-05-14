7e73150549996716864cefa623f0275d008ca78b
Tomev <rybotycki.tomasz+github@gmail.com>
2021-05-14 09:43:29 +0200

    fix(lossy_BS): Distribution calculation test moved from circuits tests to state tests.

--- tests/backends/sampling/test_circuit.py
+++ tests/backends/sampling/test_circuit.py
@@ -124,19 +124,3 @@ class TestSampling:
         initial_number_of_particles = sum(self.program.state.initial_state)
 
         assert any(sum(sample) < initial_number_of_particles for sample in samples)
-
-
-    def test_distribution(self):
-        input_state = pq.SamplingState(1, 1, 0)
-
-        U = np.asarray([[1,                         0,                        0],
-                        [0, -0.54687158 + 0.07993182j, 0.32028583 - 0.76938896j],
-                        [0,  0.78696803 + 0.27426941j, 0.42419041 - 0.35428818j]])
-
-        input_state.interferometer = U
-
-        pr = input_state.get_fock_probabilities()
-        expected_result = [0.0, 0.30545762086020883, 0.6945423895038292, 0.0, 0.0, 0.0]
-
-        assert np.allclose(pr, expected_result)
-
--- tests/backends/sampling/test_state.py
+++ tests/backends/sampling/test_state.py
@@ -124,3 +124,17 @@ class TestSamplingState:
         )
 
         assert np.allclose(program.state.interferometer, expected_interferometer)
+
+    def test_distribution(self):
+        input_state = pq.SamplingState(1, 1, 0)
+
+        U = np.asarray([[1,                         0,                        0],
+                        [0, -0.54687158 + 0.07993182j, 0.32028583 - 0.76938896j],
+                        [0,  0.78696803 + 0.27426941j, 0.42419041 - 0.35428818j]])
+
+        input_state.interferometer = U
+
+        probabilities = input_state.get_fock_probabilities()
+        expected_result = [0.0, 0.30545762086020883, 0.6945423895038292, 0.0, 0.0, 0.0]
+
+        assert np.allclose(probabilities, expected_result)
