fix(Kerr): Kerr gate scaling
Some tests were not entirely thorough on the scaling of the `Kerr` and
the `CubicPhase` gates. Additional tests were written, and it turned out
that the `Kerr` gate is not working.

The issue was, that the `kerr` calculation in `FockSimulator` and
`PureFockSimulator` was written in a way that only the first mode was
considered for a multimode calculation. This has been fixed by rewriting
the logic to consider multiple modes.

Several tests were written for testing the scalability of the `Kerr` and
`CubicPhase` gates. Furthermore, the `test_kerr_equivalency` has not
been testing the equivalence between the `FockSimulator` and
`PureFockSimulator`, therefore it has been rewritten to do that.

Additional tests have been written testing the code generation with the
`Kerr` and `CubicPhase` gates.

piquasso/_backends/fock/general/calculations.py
@@ -134,16 +134,18 @@ def annihilate(state: FockState, instruction: Instruction, shots: int) -> Result
 
 
 def kerr(state: FockState, instruction: Instruction, shots: int) -> Result:
-    mode = instruction.modes[0]
-    xi = instruction._all_params["xi"]
-    for x in xi:
+    xi_vector = instruction._all_params["xi"]
+
+    for mode_index, mode in enumerate(instruction.modes):
+        xi = xi_vector[mode_index]
+
         for index, (basis, dual_basis) in state._space.operator_basis:
             number = basis[mode]
             dual_number = dual_basis[mode]
 
             coefficient = np.exp(
                 1j
-                * x
+                * xi
                 * (number * (2 * number + 1) - dual_number * (2 * dual_number + 1))
             )
 

tests/backends/test_backend_equivalence.py
@@ -1027,22 +1027,19 @@ def test_CubicPhase_equivalence_on_multiple_modes(SimulatorClass):
         pq.FockSimulator,
     ),
 )
-def test_kerr_equivalency(SimulatorClass):
-    with pq.Program() as program_1:
+def test_Kerr_equivalence(SimulatorClass):
+    with pq.Program() as program:
         pq.Q() | pq.Vacuum()
-        pq.Q(0) | pq.Squeezing(r=0.05)
-        pq.Q(0, 1) | pq.Kerr(xi=[-1, 1])
+        pq.Q(0) | pq.Squeezing(r=0.1, phi=np.pi / 5)
+        pq.Q(1) | pq.Squeezing(r=0.2, phi=np.pi / 6)
 
-    with pq.Program() as program_2:
-        pq.Q() | pq.Vacuum()
-        pq.Q(0) | pq.Squeezing(r=0.05)
-        pq.Q(all) | pq.Kerr(xi=[-1, 1])
+        pq.Q(0, 1) | pq.Kerr(xi=[-0.1, 0.2])
 
-    generic_simulator = SimulatorClass(d=2, config=pq.Config(cutoff=10))
+    simulator = SimulatorClass(d=2)
 
-    state_1 = generic_simulator.execute(program_1).state
-    state_2 = generic_simulator.execute(program_2).state
-    fidelity = state_1.fidelity(state_2)
+    state = simulator.execute(program).state
 
-    assert np.isclose(fidelity, 1.0)
-    assert np.isclose(fidelity, state_2.fidelity(state_1))
+    assert is_proportional(
+        state.fock_probabilities,
+        [0.97613795, 0.0, 0.0, 0.01901371, 0.0, 0.00484834, 0.0, 0.0, 0.0, 0.0],
+    )