3bb726544c5caae525b67e0c67be55a0597387e4
Varga Henrik <oihsy3@inf.elte.hu>
2023-01-05 15:22:09 +0100

    FIX(fock): Fidelity precision
    **Problem**
    
    In `scipy` version 1.10.0 `sqrtm` returned with an array with type
    `complex256` but `numpy.linalg` returned with the following error:
    ```
    TypeError: array type complex256 is unsupported in linalg
    ```
    Casting it to `complex128` resulted in inaccurate fidelity values,
    however.
    
    **Solution**
    
    A less computationally demanding expression for the fidelity is used.

--- piquasso/_backends/fock/state.py
+++ piquasso/_backends/fock/state.py
@@ -17,7 +17,6 @@ from typing import Tuple, Generator, Any, Dict, List
 
 import abc
 import numpy as np
-from scipy.linalg import sqrtm
 from piquasso.api.config import Config
 from piquasso.api.state import State
 from piquasso.api.calculator import BaseCalculator
@@ -212,7 +211,7 @@ class BaseFockState(State, abc.ABC):
 
         .. math::
             \operatorname{F}(\rho_1, \rho_2) = \operatorname{Tr}(\sqrt{\sqrt{\rho_1}
-                \rho_2\sqrt{\rho_1}})^2
+                \rho_2\sqrt{\rho_1}})^2 = \operatorname{Tr}(\sqrt{\rho_1 \rho_2})^2
 
         Args:
             state: Either a :class:`~piquasso._backends.fock.pure.state.PureFockState`
@@ -222,9 +221,7 @@ class BaseFockState(State, abc.ABC):
         Returns:
             float: The calculated fidelity.
         """
-
-        sqrt_density_1 = sqrtm(self.density_matrix)
-        f = sqrtm(sqrt_density_1 @ state.density_matrix @ sqrt_density_1)
-        # Trace norm of the matrix. For more details please check:
-        # https://www.quantiki.org/wiki/trace-norm
-        return float((np.linalg.norm(f, ord="nuc") ** 2).real)
+        geometric_mean = np.sqrt(
+            np.linalg.eigvals(self.density_matrix @ state.density_matrix)
+        )
+        return np.abs(np.sum(geometric_mean)) ** 2
