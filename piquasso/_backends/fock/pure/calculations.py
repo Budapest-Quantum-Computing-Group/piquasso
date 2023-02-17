c6eb752c1406536d6fb2a73c7562a184ae5f6f09
Zoltán Kolarovszki <ifj.kolarovszki@gmail.com>
2023-02-17 14:18:31 +0100

    applying interferometer custom gradient

--- piquasso/_backends/fock/pure/calculations.py
+++ piquasso/_backends/fock/pure/calculations.py
@@ -362,55 +362,126 @@ def _get_projected_state_vector(
 
     return new_state_vector
 
-
 def _apply_passive_gate_matrix_to_state(
     state: PureFockState,
     subspace_transformations: List[np.ndarray],
     modes: Tuple[int, ...],
 ) -> None:
-    np = state._np
-    fallback_np = state._calculator.fallback_np
-
+    calculator = state._calculator
+    state_vector = state._state_vector
     space = state._space
+
+    def _apply_interferometer_matrix(state_vector, subspace_transformations):
+        state_vector = calculator.maybe_convert_to_numpy(state_vector)
+
+        subspace_transformations = [calculator.maybe_convert_to_numpy(matrix) for matrix in subspace_transformations]
+
+        index_list = _calculate_index_list_for_appling_interferometer(
+            modes, space, calculator,
+        )
+
+        new_state_vector = _calculate_state_vector_after_interferometer(
+            state_vector, subspace_transformations, index_list,
+        )
+
+        grad = _create_linear_passive_gate_gradient_function(
+            state_vector, subspace_transformations, index_list,
+        )
+        return new_state_vector, grad
+
+    wrapped = calculator.custom_gradient(_apply_interferometer_matrix)
+
+    state._state_vector = wrapped(state_vector, subspace_transformations)
+
+def _calculate_index_list_for_appling_interferometer(
+    modes: Tuple[int, ...],
+    space: FockSpace,
+    calculator,
+) -> List[np.ndarray]:
     cutoff = space.cutoff
+    d = space.d
+
     subspace = FockSpace(
-        d=len(modes), cutoff=space.cutoff, calculator=state._calculator
+        d=len(modes), cutoff=space.cutoff, calculator=calculator
     )
-    subspace_indices = [
+    auxiliary_subspace = FockSpace(
+        d=d - len(modes), cutoff=space.cutoff, calculator=calculator
+    )
+
+    indices = [
         cutoff_cardinality(cutoff=n, d=len(modes)) for n in range(cutoff + 1)
     ]
+    auxiliary_indices = [
+        cutoff_cardinality(cutoff=n, d=d-len(modes)) for n in range(cutoff + 1)
+    ]
 
-    d = state._space.d
-    auxiliary_subspace = FockSpace(
-        d=d - len(modes), cutoff=space.cutoff, calculator=state._calculator
-    )
+    auxiliary_modes = get_auxiliary_modes(d, modes)
+    all_occupation_numbers = np.zeros(d, dtype=int)
 
-    new_state_vector = [0.0] * len(state._state_vector)
+    index_list = []
 
-    auxiliary_modes = state._get_auxiliary_modes(modes)
-    all_occupation_numbers = fallback_np.zeros(d, dtype=int)
-
-    for auxiliary_occupation_numbers in auxiliary_subspace:
-        all_occupation_numbers[(auxiliary_modes,)] = auxiliary_occupation_numbers
-
-        for n in range(cutoff - sum(auxiliary_occupation_numbers)):
-            state_indices = []
-
-            matrix = subspace_transformations[n]
-
-            for column_vector_on_subspace in subspace[
-                subspace_indices[n] : subspace_indices[n + 1]
-            ]:
+    for n in range(cutoff):
+        size = indices[n + 1] - indices[n]
+        n_particle_subspace = subspace[indices[n] : indices[n + 1]]
+        auxiliary_size = auxiliary_indices[cutoff-n]
+        state_index_matrix = np.empty(shape=(size, auxiliary_size), dtype=int)
+        for idx1, auxiliary_occupation_numbers in enumerate(
+            auxiliary_subspace[: auxiliary_size]
+        ):
+            all_occupation_numbers[(auxiliary_modes,)] = auxiliary_occupation_numbers
+            for idx2, column_vector_on_subspace in enumerate(n_particle_subspace):
                 all_occupation_numbers[(modes,)] = column_vector_on_subspace
                 column_index = get_index_in_fock_space(tuple(all_occupation_numbers))
-                state_indices.append(column_index)
+                state_index_matrix[idx2, idx1] = column_index
 
-            for matrix_index, state_index in enumerate(state_indices):
-                new_state_vector[state_index] = np.dot(
-                    matrix[matrix_index], state._state_vector[state_indices]
-                )
+        index_list.append(state_index_matrix)
 
-    state._state_vector = np.stack(new_state_vector)
+    return index_list
+
+
+def _calculate_state_vector_after_interferometer(
+    state_vector: np.ndarray,
+    subspace_transformations: List[np.ndarray],
+    index_list: List[np.ndarray]
+) -> None:
+    new_state_vector = np.empty_like(state_vector)
+
+    for n, indices in enumerate(index_list):
+        new_state_vector[indices] = subspace_transformations[n] @ state_vector[indices]
+
+    return new_state_vector
+
+
+def _create_linear_passive_gate_gradient_function(
+    state_vector: np.ndarray,
+    subspace_transformations: List[np.ndarray],
+    index_list: List[np.ndarray],
+):
+    def applying_interferometer_gradient(upstream):
+        state_vector_length = len(state_vector)
+
+        initial_state_jacobian = np.zeros(shape=(state_vector_length,) * 2, dtype=state_vector.dtype)
+        matrix_jacobian = [
+            np.zeros(shape=(*matrix.shape, state_vector_length), dtype=state_vector.dtype)
+            for matrix
+            in subspace_transformations
+        ]
+
+        for n, indices in enumerate(index_list):
+            matrix = subspace_transformations[n]
+            limit, length = indices.shape
+
+            for i in range(length):
+                initial_state_jacobian[np.ix_(indices[:, i], indices[:, i])] = matrix
+
+            state_vector_slice = state_vector[indices[:limit, :]].T
+
+            for i in range(limit):
+                matrix_jacobian[n][i, :limit, indices[i, :]] = state_vector_slice
+
+        return initial_state_jacobian @ upstream, [matrix @ upstream for matrix in matrix_jacobian]
+
+    return applying_interferometer_gradient
 
 
 def _apply_active_gate_matrix_to_state(
