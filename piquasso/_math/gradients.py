Temporary fix for gradients
**Problem**

During development it became apparent, that the current gradients of the
squeezing and displacement operators are incorrect, and therefore the
CVNN layer could not converge.

**Solution**

This commit provides a temporary fix. It might not be optimal, and could
be slightly reduntant, but during experimentation in
`sf_gate_synthesis.py` it proved to be correct.

**Sources**
Gate gradients: https://quantum-journal.org/papers/q-2020-11-30-366/pdf/
TheWalrus reference: https://github.com/XanaduAI/thewalrus/blob/7bc7d9728824e4c1dd0a45ee1932c026551bde1e/thewalrus/fock_gradients.py#L171

piquasso/_math/gradients.py
@@ -45,7 +45,7 @@ def create_single_mode_displacement_gradient(
             row_sqrts * row_rolled_transformation.T
         ).T + col_sqrts * col_rolled_transformation
 
-        r_grad = phi_grad - r * transformation
+        r_grad = -r * transformation + (row_sqrts * row_rolled_transformation.T).T - col_sqrts * col_rolled_transformation
 
         phi_grad *= r * 1j
 
@@ -54,10 +54,10 @@ def create_single_mode_displacement_gradient(
 
         if static_valued:
             upstream = upstream.numpy()
-            r_grad_sum = tf.constant(np.real(np.sum(upstream * r_grad)))
+            r_grad_sum = tf.constant(np.real(np.sum(upstream * np.conj(r_grad))))
             phi_grad_sum = tf.constant(np.real(np.sum(upstream * np.conj(phi_grad))))
         else:
-            r_grad_sum = tf.math.real(tf.reduce_sum(upstream * r_grad))
+            r_grad_sum = tf.math.real(tf.reduce_sum(upstream * tf.math.conj(r_grad)))
             phi_grad_sum = tf.math.real(
                 tf.reduce_sum(upstream * tf.math.conj(phi_grad))
             )
@@ -80,7 +80,7 @@ def create_single_mode_squeezing_gradient(
         sechr = 1 / np.cosh(r)
         tanhr = np.tanh(r)
 
-        index_sqrts = np.sqrt(range(cutoff))
+        index_sqrts = np.sqrt(np.arange(cutoff, dtype=np.complex128))
 
         falling_index_sqrts = index_sqrts * np.roll(index_sqrts, 1)
 
@@ -93,30 +93,46 @@ def create_single_mode_squeezing_gradient(
         # NOTE: This algorithm rolls the last and penultimate elements of the
         # transormation matrix to the 1st and 0th place, but the 0th and 1st element of
         # `row_sqrts` and `col_sqrts` is always zero, so it is fine.
-        phi_grad = (
+        phi_grad = -0.5j * tanhr * ((
             row_sqrts * row_rolled_transformation.T
-        ).T + col_sqrts * col_rolled_transformation
+        ).T + col_sqrts * col_rolled_transformation)
 
         diagonally_rolled_transformation = np.roll(transformation, (1, 1), axis=(0, 1))
 
-        r_grad = -(
-            tanhr / 2 * transformation
-            + (sechr**2) / 2 * phi_grad
-            + np.outer(index_sqrts, index_sqrts)
-            * sechr
-            * tanhr
-            * diagonally_rolled_transformation
-        )
+        r_grad = (
+            (-tanhr * 0.5) * transformation
+            - (sechr * tanhr) * np.outer(index_sqrts, index_sqrts) * diagonally_rolled_transformation
+            - (sechr**2 * 0.5) * (row_sqrts * row_rolled_transformation.T).T
+            + (sechr**2 * 0.5) * (col_sqrts * col_rolled_transformation)
+            )
+        """
+        T = transformation
+        grad_r = np.zeros((cutoff, cutoff), dtype=np.complex128)
+        grad_phi = np.zeros((cutoff, cutoff), dtype=np.complex128)
 
-        phi_grad *= -tanhr / 2 * 1j
+        eiphi = np.exp(1j * phi)
+        eiphiconj = np.exp(-1j * phi)
 
+        for m in range(cutoff):
+            for n in range(cutoff):
+                grad_r[m, n] = (
+                    -0.5 * tanhr * T[m, n]
+                    - sechr * tanhr * index_sqrts[m] * index_sqrts[n] * T[m - 1, n - 1]
+                    - 0.5 * eiphi * sechr**2 * index_sqrts[m] * index_sqrts[m - 1] * T[m - 2, n]
+                    + 0.5 * eiphiconj * sechr**2 * index_sqrts[n] * index_sqrts[n - 1] * T[m, n - 2]
+                )
+                grad_phi[m, n] = (
+                    -0.5j * eiphi * tanhr * index_sqrts[m] * index_sqrts[m - 1] * T[m - 2, n]
+                    - 0.5j * eiphiconj * tanhr * index_sqrts[n] * index_sqrts[n - 1] * T[m, n - 2]
+                )
+        """
         tf = calculator._tf
 
         static_valued = tf.get_static_value(upstream) is not None
 
         if static_valued:
             upstream = upstream.numpy()
-            r_grad_sum = tf.constant(np.real(np.sum(upstream * r_grad)))
+            r_grad_sum = tf.constant(np.real(np.sum(upstream * np.conj(r_grad))))
             phi_grad_sum = tf.constant(np.real(np.sum(upstream * np.conj(phi_grad))))
         else:
             # NOTE: Possibly Tensorflow bug, cast needed.
@@ -124,7 +140,7 @@ def create_single_mode_squeezing_gradient(
             #  a double tensor but is a float tensor [Op:AddN].
             # The bug does not occur with Displacement gradient for unknown reasons.
             r_grad_sum = tf.cast(
-                tf.math.real(tf.reduce_sum(upstream * r_grad)), tf.float32
+                tf.math.real(tf.reduce_sum(upstream * tf.math.conj(r_grad))), tf.float32
             )
             phi_grad_sum = tf.math.real(
                 tf.reduce_sum(upstream * tf.math.conj(phi_grad))