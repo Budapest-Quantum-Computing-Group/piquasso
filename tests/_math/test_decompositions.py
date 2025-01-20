d506d30f47c4fc3260ee7b0a01a93c7dc3535aa5
Zoltán Kolarovszki <ifj.kolarovszki@gmail.com>
2025-01-20 14:01:54 +0100

    chore: Update copyright

--- tests/_math/test_decompositions.py
+++ tests/_math/test_decompositions.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2021-2024 Budapest Quantum Computing Group
+# Copyright 2021-2025 Budapest Quantum Computing Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
