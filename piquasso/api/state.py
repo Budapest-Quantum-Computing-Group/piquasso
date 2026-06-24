a07512d38b69eca9deceddc66594b4cb79a5d12d
Zoltán Kolarovszki <ifj.kolarovszki@gmail.com>
2026-06-24 19:52:34 +0200

    fix importerror

--- piquasso/api/state.py
+++ piquasso/api/state.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Tuple, Type, Optional, Self
+from typing import Any, Tuple, Type, Optional
+from typing_extensions import Self
 
 import abc
 import copy
